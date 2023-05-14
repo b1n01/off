40577463a8c53a876fb5579af447881209b55fa9
b1n01 <chielorenz@gmail.com>
2023-05-14 18:07:05 +0200

    Add eslint and tailwind

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Header from "@/app/Header";
+import "@/style/globals.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
