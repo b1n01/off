da3fc56451767e2a524c9cd0f2cba21d21858de8
b1n01 <chielorenz@gmail.com>
2023-05-16 23:48:06 +0200

    Comment middleware

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -3,14 +3,36 @@ import type { NextRequest } from "next/server";
 import { getData } from "@/lib/server/session";
 
 export async function middleware(request: NextRequest) {
+  // Should I math routes programmatically instead of using
+  // the "mather" config???
+  // See https://nextjs.org/docs/app/building-your-application/routing/middleware#conditional-statements
+
+  // if (request.nextUrl.pathname.startsWith('/about')) {
+  //   return NextResponse.rewrite(new URL('/about-2', request.url));
+  // }
+
   const session = await getData({ request });
   if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 }
 
 export const config = {
+  /**
+   * This matches every routes except for:
+   *
+   * Nextjs routes:
+   *  "/_next/static"
+   *  "/_next/image"
+   *  "/favicon.ico"
+   *
+   * Public routes:
+   *  "/" <- $ is the end of the string, so just the root /
+   *  "/login"
+   *
+   * See https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+   */
   matcher: [
-    "/((?!login|api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!login|$|_next/static|_next/image|favicon.ico).*)",
   ],
 };
