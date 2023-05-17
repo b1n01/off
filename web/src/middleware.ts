1dcab3b0e3af73bdd1376f59bdca02c7155feb7a
b1n01 <chielorenz@gmail.com>
2023-05-18 00:14:53 +0200

    Check path programmatically

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,38 +1,25 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { getData } from "@/lib/server/session";
 
+const publicPaths = [
+  // NextJs paths
+  new RegExp("^/_next/(image|static).*"),
+  new RegExp("^/favicon.ico$"),
+
+  // Public routes
+  new RegExp("^/login$"),
+  new RegExp("^/$"),
+];
+
 export async function middleware(request: NextRequest) {
-  // Should I math routes programmatically instead of using
-  // the "mather" config???
-  // See https://nextjs.org/docs/app/building-your-application/routing/middleware#conditional-statements
+  const isPublicPath = publicPaths.some((path) =>
+    path.test(request.nextUrl.pathname)
+  );
 
-  // if (request.nextUrl.pathname.startsWith('/about')) {
-  //   return NextResponse.rewrite(new URL('/about-2', request.url));
-  // }
-
-  const session = await getData({ request });
-  if (!session) {
-    return NextResponse.redirect(new URL("/", request.url));
+  if (!isPublicPath) {
+    const session = await getData({ request });
+    if (!session) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
 }
-
-export const config = {
-  /**
-   * This matches every routes except for:
-   *
-   * Nextjs routes:
-   *  "/_next/static"
-   *  "/_next/image"
-   *  "/favicon.ico"
-   *
-   * Public routes:
-   *  "/" <- $ is the end of the string, so just the root /
-   *  "/login"
-   *
-   * See https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-   */
-  matcher: [
-    "/((?!login|$|_next/static|_next/image|favicon.ico).*)",
-  ],
-};
