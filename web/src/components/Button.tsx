dda857c6fe2eb05b63df1a33746016fef0e7d5b4
b1n01 <chielorenz@gmail.com>
2023-05-22 00:18:08 +0200

    Update Button

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,9 +1,22 @@
-import { ReactNode } from "react";
+import { type ReactNode } from "react";
+import Link from "next/link";
 
-export function Button({ children }: { children: ReactNode }) {
-  return (
-    <button className="rounded px-4 py-2 text-sm font-bold bg-violet-700 hover:bg-violet-600 active:bg-violet-500">
-      {children}
-    </button>
-  );
+export function Button(
+  { children, className = "", href = "", useLink = true }: {
+    children: ReactNode;
+    className?: string;
+    href?: string;
+    useLink?: boolean;
+  },
+) {
+  const props = {
+    children,
+    href,
+    className: `rounded px-4 py-2 text-center text-sm font-bold bg-violet-700 
+    hover:bg-violet-600 active:bg-violet-500 ${className}`,
+  };
+
+  return props.href
+    ? useLink ? <Link {...props} /> : <a {...props} />
+    : <button {...props} />;
 }
