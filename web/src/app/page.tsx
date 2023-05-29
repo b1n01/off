e2340d386d9dd70b3ee50dec759a9a75f6e85192
b1n01 <chielorenz@gmail.com>
2023-05-29 19:56:27 +0200

    Restore landing page placeholder

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,23 @@
+import { getData, getToken } from "@/lib/server/session";
+import { withRepo } from "@/lib/server/repo";
 import { Header } from "@/components/Header";
+import { Button } from "@/components/Button";
 import Feed from "@/app/(board)/feed/page";
 
 export default async function Home() {
+  const session = await getData();
+
+  if (!session) {
+    return (
+      <div className="text-center">
+        <div className="m-8">
+          This will be an awesome landing page, for now just sign in:
+        </div>
+        <Button href="/login">Sign in</Button>
+      </div>
+    );
+  }
+
   return (
     <>
       <Header />
